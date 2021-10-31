2d1b7c7e7513621576fd806e9af9a9b79b99af0a
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-01 01:17:45 +0530

    ADDED: Greeting DTO bean

---
+++ src/main/java/com/bridgelabz/messagingapp/model/Greeting.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.messagingapp.model;
+
+public class Greeting {
+	private long GreetingId;
+	private String name;
+
+	public Greeting(long incrementAndGet, String name) {
+		setGreetingId(incrementAndGet);
+		setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getGreetingId() {
+		return GreetingId;
+	}
+
+	public void setGreetingId(long greetingId) {
+		GreetingId = greetingId;
+	}
+
+}
